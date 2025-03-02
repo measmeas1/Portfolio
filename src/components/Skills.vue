8d4907a6495ff273ba4f93d1cfc0615f43fd7080
measmeas1 <mabmeas12@gmail.com>
2025-03-02 17:07:34 +0700

    fix

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -9,7 +9,7 @@
     <div class="mt-20 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 text-center">
       <div class="flex flex-col items-center" v-for="skill in skills" :key="skill.name">
         <div class="skill-box p-4 rounded-xl transition duration-300 ease-in-out transform hover:scale-125">
-          <img :src="`/assets${skill.icon}`" :alt="skill.name" class="w-16 h-16 mb-4" />
+          <img :src="`${skill.icon}`" :alt="skill.name" class="w-16 h-16 mb-4" />
           <div class="absolute inset-0 bg-green-400 opacity-10 rounded-full blur-lg"></div>
         </div>
         <span class="text-xl font-semibold ">{{ skill.name }}</span>
@@ -25,16 +25,16 @@ export default {
   data() {
     return {
       skills: [
-        { name: "HTML", icon: "/html5.svg" },
-        { name: "CSS", icon: "/css3.svg" },
-        { name: "JavaScript", icon: "/javascript.svg" },
-        { name: "Vue.js", icon: "/vue.svg" },
-        { name: "Tailwind CSS", icon: "/tailwind.svg" },
-        { name: "Java", icon: "/java.svg" },
-        { name: "Node.js", icon: "/node.svg" },
-        { name: "MySQL", icon: "/mysql.svg" },
-        { name: "PostgreSQL", icon: "/postgresql.svg" },
-        { name: "GitHub", icon: "/github.svg" },
+        { name: "HTML", icon: "../assets/html5.svg" },
+        { name: "CSS", icon: "../assets/css3.svg" },
+        { name: "JavaScript", icon: "../assets/javascript.svg" },
+        { name: "Vue.js", icon: "../assets/vue.svg" },
+        { name: "Tailwind CSS", icon: "../assets/tailwind.svg" },
+        { name: "Java", icon: "../assets/java.svg" },
+        { name: "Node.js", icon: "../assets/node.svg" },
+        { name: "MySQL", icon: "../assets/mysql.svg" },
+        { name: "PostgreSQL", icon: "../assets/postgresql.svg" },
+        { name: "GitHub", icon: "../assets/github.svg" },
       ],
     };
   },
